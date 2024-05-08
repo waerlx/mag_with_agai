49c03cb42846c27b710c233862cb837f7b59e25c
Adelina <adelinaakylbekova565@gmail.com>
2024-05-08 14:06:31 +0600

    add element for footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,30 @@
 import './Footer.css';
+import { BrowserRouter, Link, Router } from 'react-router-dom';
 function Footer() {
     return (
         <div className="footer">
-            <Footer/>
-            
+
+            <div className="container">
+                <div className="footer_wrap">
+                    <div className="links-acc">
+                        <div className="footer-links-top">
+
+                        </div>
+                        <div className="footer-links-bottom flex">
+                            <Router>
+                                <Link className='footer-link-ic'></Link>
+                                <Link className='footer-link-ic'></Link>
+                                <Link className='footer-link-ic'></Link>
+                                <Link className='footer-link-ic'></Link>
+                            </Router>
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
         </div>
     );
 }
