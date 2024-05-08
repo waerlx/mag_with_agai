9b2e8c3d6cd5623ab262a81c1c62c0b693aa62a8
Adelina <adelinaakylbekova565@gmail.com>
2024-05-08 17:11:03 +0600

    correct contact

--- src/Pages/Contacts/Contacts.js
+++ src/Pages/Contacts/Contacts.js
@@ -4,10 +4,12 @@ function Contacts() {
     return (
         <>
             <div className="contacts">
+                <div className="container">
 
-                <h2 className='contact_tit'>Contacts</h2>
-                <h3 className='contact_tit'><b>Служба поддержки читателей</b></h3>
-                <Typography className='contact_txt'>Если у вас возникли проблемы при использовании ЛитРес, воспользуйтесь нашей базой знаний  или свяжитесь со службой поддержки:</Typography>
+                    <h2 className='contact_tit'>Contacts</h2>
+                    <h3 className='contact_tit'><b>Служба поддержки читателей</b></h3>
+                    <Typography className='contact_txt'>Если у вас возникли проблемы при использовании ЛитРес, воспользуйтесь нашей базой знаний  или свяжитесь со службой поддержки:</Typography>
+                </div>
             </div>
         </>
     );
