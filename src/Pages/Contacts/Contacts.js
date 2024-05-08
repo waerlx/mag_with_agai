06a0511cdfd97ec64d764fd2006213ecd3bafbe6
Adelina <adelinaakylbekova565@gmail.com>
2024-05-08 14:07:03 +0600

    add img

--- src/Pages/Contacts/Contacts.js
+++ src/Pages/Contacts/Contacts.js
@@ -1,9 +1,15 @@
+import { Typography } from 'antd';
 import './Contacts.css';
 function Contacts() {
     return (
-        <div className="contacts">
-            <h1>Contacts</h1>
-        </div>
+        <>
+            <div className="contacts">
+
+                <h2 className='contact_tit'>Contacts</h2>
+                <h3 className='contact_tit'><b>Служба поддержки читателей</b></h3>
+                <Typography className='contact_txt'>Если у вас возникли проблемы при использовании ЛитРес, воспользуйтесь нашей базой знаний  или свяжитесь со службой поддержки:</Typography>
+            </div>
+        </>
     );
 }
 export default Contacts;
