11783ea629d5bca36b0d9868c4438dc4014a18de
Adelina <adelinaakylbekova565@gmail.com>
2024-05-08 14:04:43 +0600

    add page for app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Menu from './components/Menu/Menu';
+import Header from './components/Header/Header';
+import  Footer  from './components/Footer/Footer';
+import About from './Pages/About/About';
+import Contacts from './Pages/Contacts/Contacts';
 
 function App() {
   return (
     <>
-      <Menu/>
+    <Menu />
+      {/* <About /> */}
+      <Contacts/>
     </>
 
   )
