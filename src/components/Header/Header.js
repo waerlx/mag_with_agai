b27becd5fb9a3d4b159defa0e43be864ee5ccea1
Adelina <adelinaakylbekova565@gmail.com>
2024-05-08 14:06:49 +0600

    add header element

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,16 +2,11 @@ import './Header.css';
 import {Image} from 'antd';
 import Menu from '../Menu/Menu';
 import { Link } from 'react-router-dom';
-import logo from "../../../public/Img/icons/logo"
 function Header() {
     return (
         <div>
-            <header>
-            <Link>
-            <Image src={logo} width={100} height={31} />
-            </Link>
-           <Menu />
-           </header>
+            
+
 
         </div>
     )
